025bc3eac5ad4c06a7ab571f9c6b4d83eea80a8e
ehmoji <73181037+ehmoG@users.noreply.github.com>
2025-02-26 17:17:45 +0900

    25.02.26 HelloWorld
    1차 커밋

---
+++ Assets/Scripts/HelloWorld/HelloWorldPractice.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class HelloWorldPractice : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello Unity!!!\n123456789");
+    }
+}
